cc9881903a27f74fb65a157950d504799389a94f
unknown <din4eg@gmail.com>
2020-12-13 22:08:12 +0200

    before full responsivity

--- js/canvas.js
+++ js/canvas.js
@@ -29,6 +29,7 @@ function createMeme(gCurrId) {
         size: 44,
         font:'Impact',
         align: 'center',
+        strokeColor: 'black',
         color: 'white'
       }
     ]
@@ -74,7 +75,7 @@ function drawTextFromGMeme(x = 250, y = 50) {
   for (var i = 0; i < gMeme.lines.length; i++) {
     var text = gMeme.lines[i].txt;
     gCtx.lineWidth = '3'
-    gCtx.strokeStyle = 'black'
+    gCtx.strokeStyle = gMeme.lines[gMeme.selectedLineIdx].strokeColor
     gCtx.fillStyle = gMeme.lines[i].color
     // same: gCtx.font = 'gMeme.lines[0].size, Impact'
     var sizeFont = '' + gMeme.lines[i].size + 'px' + ' ' + gMeme.lines[i].font;
@@ -84,7 +85,7 @@ function drawTextFromGMeme(x = 250, y = 50) {
     gCtx.strokeText(text, gMeme.lines[i].x, gMeme.lines[i].y)
     gCtx.fillText(text, gMeme.lines[i].x, gMeme.lines[i].y)
     if(i === gMeme.selectedLineIdx){
-      drawRect(20, gMeme.lines[i].y-50 )
+      drawRect(20, gMeme.lines[i].y-45 )
     }
   }
 }
@@ -126,9 +127,8 @@ function drawRect(x, y) {
   gCtx.rect(x, y, gCanvas.width - (x * 2), 60) // x,y,widht,height
   gCtx.lineWidth = '1'
   gCtx.stroke()
-  // gCtx.globalAlpha = 0.8;
-  // gCtx.fillStyle = 'white';
-  // gCtx.fillRect(x, y, 44, 44)
+  gCtx.fillStyle = 'rgba(255, 255, 255, 0.2)';
+  gCtx.fillRect(x, y, gCanvas.width - (x * 2), 60)
 }
 
 function addRow() {
@@ -154,6 +154,26 @@ function changeRow() {
   gCurrRow=gMeme.selectedLineIdx;
   if (gCurrRow< gMeme.lines.length-1) gMeme.selectedLineIdx++;
   else gMeme.selectedLineIdx=0;
+  clearCanvas();
+  drawTextFromGMeme();
+}
+
+function onStrokeClick(){
+  document.querySelector('.stroke-color').click()
+}
+
+function onFillClick(){
+  document.querySelector('.fill-color').click()
+}
+
+function setStrokeColor(color) {
+  gMeme.lines[gMeme.selectedLineIdx].strokeColor = color;
+  clearCanvas();
+  drawTextFromGMeme();
+}
+
+function setFillColor(fillColor) {
+  gMeme.lines[gMeme.selectedLineIdx].color = fillColor;
   clearCanvas();
   drawTextFromGMeme();
 }
